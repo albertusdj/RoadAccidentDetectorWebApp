a1493db4864d51960f76c2f5d3fc6f94fbe4dbcd
Albertdj <albertusdjauharidjohan@gmail.com>
2018-11-12 00:13:25 +0700

    create predict

--- roadsite/accidentDetector/views.py
+++ roadsite/accidentDetector/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+import os
+import pickle
+
+model = pickle.load(open(os.getcwd() + "\models" + "\multinomialNB"), "r")
 
 # Create your views here.
 def index(request):
   context = {}
   return render(request, 'accidentDetector/index.html', context)
+
+def predict(request):
+  tweet = request.get_json()['tweet']
+
+  prediction = model.predict(tweet)
+  response = {}
+  response['prediction'] = prediction
+
+  return JsonResponse(response)
