c734ccac7ff58f2ac9080ea24f05bc05fc33114e
Christopher Clement Andreas <clement.andreas97@gmail.com>
2018-11-12 19:51:29 +0700

    added locationPredictor class for location prediction; added ner models

--- roadsite/accidentDetector/namedEntityRecognition.py
+++ roadsite/accidentDetector/namedEntityRecognition.py
@@ -22,6 +22,11 @@ def readTweetForTesting(file):
     data = pd.DataFrame({'tweets':csv['tweets'], 'isRoadIncident':csv['isRoadIncident']})[['tweets', 'isRoadIncident']]
     return data
 
+def saveModel(model, file):
+    output = open('models/%s' % file, 'wb')
+    pkl.dump(model, output)
+    output.close()
+
 def loadModel(file):
     input = open('models/%s' % file, 'rb')
     data = pkl.load(input)
@@ -70,8 +75,7 @@ def extractFeatureFromWords(sent, i):
 
     return features
 
-
-def extractFeatureFromSentences(sent):
+def extractFeatureFromSentence(sent):
     return [extractFeatureFromWords(sent, i) for i in range(len(sent))]
 
 def labelsExtraction(sent):
@@ -85,10 +89,10 @@ def featureExtraction(data):
     train_sentences = data[:10000]
     test_sentences = data[10000:20000]
 
-    X_train = [extractFeatureFromSentences(sentence) for sentence in train_sentences]
+    X_train = [extractFeatureFromSentence(sentence) for sentence in train_sentences]
     y_train = [labelsExtraction(sentence) for sentence in train_sentences]
 
-    X_test = [extractFeatureFromSentences(sentence) for sentence in test_sentences]
+    X_test = [extractFeatureFromSentence(sentence) for sentence in test_sentences]
     y_test = [labelsExtraction(sentence) for sentence in test_sentences]
 
     return (X_train, y_train, X_test, y_test)
@@ -126,7 +130,7 @@ def predictLocation(tweets):
     for i in range(0, len(tweets)):
         if y_pred[i] == 1:
             tagged = postag(tweets[i])
-            x = [extractFeatureFromSentences(s) for s in tagged]
+            x = [extractFeatureFromSentence(s) for s in tagged]
             y = crf.predict(x)
             y_loc = locationExtraction(tagged,y)
             if len(y_loc) > 0:
@@ -139,6 +143,103 @@ def predictLocation(tweets):
     df = pd.DataFrame({'tweets':tweets, 'isRoadIncident':y_pred, 'location':locations})[['tweets', 'isRoadIncident', 'location']]
     
     return df
+
+class locationPredictor():
+    crf = None
+
+    def __init__(self, file):
+        self.crf = loadModel(file)
+    
+    @staticmethod
+    def extractFeatureFromWords(sent, i):
+        word = sent[i][0]
+        postag = sent[i][1]
+
+        features = {
+            'bias': 1.0,
+            'lower': word.lower(),
+            'word[-3:]': word[-3:],
+            'word[-2:]': word[-2:],
+            'isSupper': word.isupper(),
+            'isTitle': word.istitle(),
+            'isDigit': word.isdigit(),
+            'postag': postag,
+            'postag[:2]': postag[:2],
+        }
+        if i > 0:
+            word1 = sent[i-1][0]
+            postag1 = sent[i-1][1]
+            features.update({
+                '-1:lower': word1.lower(),
+                '-1:isTitle': word1.istitle(),
+                '-1:isSuper': word1.isupper(),
+                '-1:postag': postag1,
+                '-1:postag[:2]': postag1[:2],
+            })
+        else:
+            features['BOS'] = True
+
+        if i < len(sent)-1:
+            word1 = sent[i+1][0]
+            postag1 = sent[i+1][1]
+            features.update({
+                '+1:lower': word1.lower(),
+                '+1:isTitle': word1.istitle(),
+                '+1:isSuper': word1.isupper(),
+                '+1:postag': postag1,
+                '+1:postag[:2]': postag1[:2],
+            })
+        else:
+            features['EOS'] = True
+
+        return features
+
+    @staticmethod
+    def extractFeatureFromSentence(sent):
+        return [extractFeatureFromWords(sent, i) for i in range(len(sent))]
+
+    @staticmethod
+    def postag(tweet):
+        sent_text = nltk.sent_tokenize(tweet)
+        sent = list()
+        for sentence in sent_text:
+            tokenized_text = nltk.word_tokenize(sentence)
+            tagged = nltk.pos_tag(tokenized_text)
+            sent.append(tagged)
+        return sent
+
+    @staticmethod
+    def locationExtraction(tagged, y):
+        locations = []
+        for i in range (0, len(y)):
+            loc = ''
+            for j in range(0, len(y[i])):
+                if y[i][j] == 'B-geo':
+                    if len(loc) == 0:
+                        loc += tagged[i][j][0]
+                    else:
+                        locations.append(loc)
+                        loc = tagged[i][j][0]
+                if y[i][j] == 'I-geo':
+                    loc += ' ' + tagged[i][j][0]
+            if len(loc) > 0:
+                locations.append(loc)
+        return locations
+
+    @staticmethod
+    def predictLocation(model, tweets):        
+        locations = []
+        for i in range(0, len(tweets)):
+            tagged = postag(tweets[i])
+            x = [extractFeatureFromSentence(s) for s in tagged]
+            y = model.predict(x)
+            y_loc = locationExtraction(tagged,y)
+            if len(y_loc) > 0:
+                locations.append(y_loc) 
+            else:
+                locations.append('-')
+        
+        return locations
 
 if __name__ == '__main__':
     preprocessor = Preprocessor()
@@ -167,6 +268,7 @@ if __name__ == '__main__':
     labels.remove('O')
 
     y_pred = crf.predict(X_test)
+    # print(y_pred)
     print(metrics.flat_f1_score(y_test, 
                           y_pred,
                           average='weighted', 
@@ -177,7 +279,10 @@ if __name__ == '__main__':
     pipelineModelFile = 'multinomialNB.pkl'
     pipeline = loadModel(pipelineModelFile)
     result = predictLocation(tweetsTestData)
-    print(result)
+    # print(result)
+
+    # Save Model
+    saveModel(crf, 'namedEntityRecognition.pkl')
 
 
 
